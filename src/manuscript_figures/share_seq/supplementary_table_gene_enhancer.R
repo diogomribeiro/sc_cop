62e05eea0640b2672bc87e189dcbb16a6b75802d
Diogo Ribeiro <diogo.ribeiro@unil.ch>
2022-07-21 16:57:46 +0200

    cleaning repository

--- src/manuscript_figures/share_seq/supplementary_table_gene_enhancer.R
+++
@@ -1,5 +0,0 @@
-
-library(ggplot2)
-library(data.table)
-
-data = fread( "zcat /home/dribeiro/git/sc_cop/data/git/sc_cop/data/share_seq_gene_enhancer_associations.tsv.gz", stringsAsFactors = FALSE, header = T, sep="\t")
